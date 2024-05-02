8ec6df5672a47a1a94cf7142124b3a333c0a3057
guilherm-dev <bascaligui@gmail.com>
2024-05-02 10:37:32 -0300

    new update

--- stop watch/src/StopWatch.java
+++ stop watch/src/StopWatch.java
@@ -1,6 +1,77 @@
+import java.util.Scanner;
+
 public class StopWatch {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Menu();
+        scanner.close();
     }
+
+    static void Menu() {
+        clearConsole();
+        System.out.println("S = seg -> 10s = 10 seconds");
+        System.out.println("M = min -> 1m = 1 minute");
+        System.out.println("0 = Exit");
+        System.out.println("How many minutes do you want to count?");
+
+        String date = scanner.nextLine().toLowerCase();
+        char type = date.charAt(date.length() - 1);
+        int time = Integer.parseInt(date.substring(0, date.length() - 1));
+        int multiplier = 1;
+
+        if (type == 'm') {
+            multiplier = 60;
+        }
+        if (time == 0) {
+            System.exit(0);
+        }
+
+        preStart(time * multiplier);
+    }
+
+    static void preStart(int time) {
+        clearConsole();
+        System.out.println("Ready...");
+        sleep(1000);
+
+        System.out.println("Set...");
+        sleep(1000);
+
+        System.out.println("Go...");
+        sleep(1500);
+
+        start(time);
+    }
+
+    static void start(int time) {
+        int currentTime = 0;
+
+        while (currentTime != time) {
+            clearConsole();
+            currentTime++;
+            System.out.println(currentTime);
+            sleep(1000);
+        }
+
+        clearConsole();
+        System.out.println("Stop Watch finished");
+        sleep(2500);
+        Menu();
+    }
+
+    static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static void sleep(int milliseconds) {
+        long endTime = System.currentTimeMillis() + milliseconds;
+        while (System.currentTimeMillis() < endTime) {
+            @SuppressWarnings("unused")
+            int dummy = 0;
+        }
+    }
+
+
 }

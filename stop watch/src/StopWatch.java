16747fc3e1b461ab1d125e769574b1587f925cc7
guilherm-dev <bascaligui@gmail.com>
2024-05-03 13:52:31 -0300

    new update

--- stop watch/src/StopWatch.java
+++ stop watch/src/StopWatch.java
@@ -11,6 +11,8 @@ public class StopWatch {
     static void Menu() {
         System.out.println("S = seg -> 10s = 10 seconds");
         System.out.println("M = min -> 1m = 1 minute");
+        System.out.println("H = hour -> 1h = 1 hour");
+        System.out.println("D = day -> 1d = 1 day");
         System.out.println("0 = Exit");
         System.out.println("How many minutes do you want to count?");
 
@@ -19,11 +21,23 @@ public class StopWatch {
         int time = Integer.parseInt(date.substring(0, date.length() - 1));
         int multiplier = 1;
 
-        if (type == 'm') {
-            multiplier = 60;
-        }
-        if (time == 0) {
-            System.exit(0);
+        switch (type) {
+            case 's':
+                multiplier = 1000;
+                break;
+            case 'm':
+                multiplier = 1000 * 60;
+                break;
+            case 'h':
+                multiplier = 1000 * 3600;
+                break;
+            case 'd':
+                multiplier = 1000 * 86400;
+                break;
+            default:
+                System.out.println("Invalid input. Please try again.");
+                Menu();
+                return;
         }
 
         preStart(time * multiplier);
